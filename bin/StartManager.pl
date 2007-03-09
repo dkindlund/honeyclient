caa3ce829df6792dfb52a6c08a736596820b35cd
Darien Kindlund <github@kindlund.com>
2007-03-09 20:04:44 +0000

    sc: merging branch using tags svn+ssh://kindlund@www.honeyclient.org/home/svn/honeyclient/honeyclient/tags/exp/PRE-flindiakos-multi_urls and svn+ssh://kindlund@www.honeyclient.org/home/svn/honeyclient/honeyclient/branches/exp/flindiakos-multi_urls

--- bin/StartManager.pl
+++ bin/StartManager.pl
@@ -14,20 +14,60 @@ use Storable qw(nfreeze thaw);
 # Include Base64 Libraries
 use MIME::Base64 qw(encode_base64 decode_base64);
 
+# Include Getopt Parser
+use Getopt::Long;
+
 use HoneyClient::Manager;
 
 # We expect that the user will supply a single argument to this script.
 # Namely, the initial URL that they want the Agent to use.
+# They can however supply multiple urls which will be processed in order
+
+my $driver = "IE";
+my $config = "/vm/Agent.Master-7/winXPPro.cfg";
+my $maxrel = 5;
+my $nexturl = "";
+my $urllist= "";
+
+# TODO: Need --help option, along with sanity checking.
+# TODO: Also need a decent POD for this code.
+GetOptions('driver=s'             => \$driver,
+           'master_vm_config=s'   => \$config,
+           'url_list=s'           => \$urllist,
+           'max_relative_links:i' => \$maxrel);
+
+# Go through the list of urls to create the array
+# Anything not associated with an option is a URL
+# Grab those first and then get the ones from the file specified
+my @urls;
+push( @urls, @ARGV ); 
+if( -e $urllist ){
+    open URL, $urllist;
+    push(@urls, <URL>);
+}
+
+# Get the first url from the list
+# Create a hashtable in the form: url => 1 for links_to_visit 
+chomp @urls;
+my $firsturl = shift @urls;
+my %remaining_urls;
+foreach(@urls){
+    # We assign our initial list of URLs a priority of 1000, so that
+    # they'll be (likely to be) selected first, before going to any other
+    # external URLs found from subsequent drive operations.
+    $remaining_urls{$_} = 1000;
+}
 
 my $agentState = HoneyClient::Manager->run(
-                    driver           => 'IE', # Change to 'IE' or 'FF'
-                    master_vm_config => '/vm/Agent.Master-7/winXPPro.cfg',
+                    driver           => $driver, # Change to 'IE' or 'FF'
+                    master_vm_config => $config,
                     agent_state      => encode_base64(nfreeze({
-                        IE => { # Change to 'IE' or 'FF'
-                            next_link_to_visit => $ARGV[0],
+                        $driver => { # Change to 'IE' or 'FF'
+                            next_link_to_visit => $firsturl,
                             # Enable this line, if you want to only go to the
                             # first 5 links for each domain.
-                            max_relative_links_to_visit => 5,
+                            max_relative_links_to_visit => $maxrel,
+                            links_to_visit => \%remaining_urls,
                          },
                     })), 
                  );
