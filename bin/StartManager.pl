6b76fd3853f880f75aec105b7dd91e309c54bad7
Darien Kindlund <github@kindlund.com>
2007-03-14 20:00:08 +0000

    Integration testing.

--- bin/StartManager.pl
+++ bin/StartManager.pl
@@ -24,7 +24,7 @@ use HoneyClient::Manager;
 # They can however supply multiple urls which will be processed in order
 
 my $driver = "IE";
-my $config = "/vm/Agent.Master-7/winXPPro.cfg";
+my $config = "/vm/master-vms/Agent.Master-9/winXPPro.cfg";
 my $maxrel = 5;
 my $nexturl = "";
 my $urllist= "";
