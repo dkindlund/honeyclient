4e9595c9ac6858854a3201d96dbf8221e9b7edc5
Darien Kindlund <github@kindlund.com>
2008-01-10 21:28:11 +0000

    Integration testing.

--- lib/HoneyClient/Manager/Database.pm
+++ lib/HoneyClient/Manager/Database.pm
@@ -1,22 +1,22 @@
 use strict;
 
-use YAML qw'freeze thaw Bless';
-use XML::RPC;
-
 package HoneyClient::Manager::Database;
+
+use YAML qw'freeze';
+use XML::RPC;
 
 sub insert {
 	my ($class,$obj) = @_;
 	my $obj_yaml = freeze($obj);
 
 	# Execute insert via XML-RPC and return id
-	eval {
-		my $xmlrpc = XML::RPC->new('http://localhost:3000/hc_database/api');
+	#eval {
+		my $xmlrpc = XML::RPC->new('http://172.16.164.103:3000/hc_database/api');
 		return $xmlrpc->call("insert",$class,$obj_yaml);
-	};
-	if ($@) {
-		return 0;
-	}
+	#};
+	#if ($@) {
+	#	return 0;
+	#}
 }
 
 #TODO: Implement this method to retrieve web links to visit.
@@ -27,3 +27,5 @@ sub retrieve_urls {
 #TODO: Implement this method to submit a list of web links to the queue.
 sub submit_urls {
 }
+
+1;
