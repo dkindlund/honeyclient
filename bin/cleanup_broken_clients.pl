19e73131de0d6e5cf85534bbcad90363c4322691
Darien Kindlund <github@kindlund.com>
2008-02-22 20:13:25 +0000

    Updated cleanup code.

--- bin/cleanup_broken_clients.pl
+++ bin/cleanup_broken_clients.pl
@@ -22,21 +22,20 @@ my $snapshot_path = getVar(name => "snapshot_path", namespace => "HoneyClient::M
 
 my $clients = HoneyClient::Manager::Database::get_broken_clients($args);
 
-#my ($cid, $id);
 while (my ($cid, $id) = each %{$clients}) {
 
     $LOG->info("Deleting VM (" . $cid . ")");
 
-    print "Executing: vmware-cmd -s unregister " . $datastore_path . "/" . $cid . "/*.vmx\n";
+    $LOG->info("Executing: vmware-cmd -s unregister " . $datastore_path . "/" . $cid . "/*.vmx");
     system("vmware-cmd -s unregister " . $datastore_path . "/" . $cid . "/*.vmx");
 
-    print "Executing: vmware-cmd -s unregister " . $datastore_path . "/" . $cid . "/*.cfg\n";
+    $LOG->info("Executing: vmware-cmd -s unregister " . $datastore_path . "/" . $cid . "/*.cfg");
     system("vmware-cmd -s unregister " . $datastore_path . "/" . $cid . "/*.cfg");
     
-    print "Executing: rm -rf " . $datastore_path . "/" . $cid . "\n";
+    $LOG->info("Executing: rm -rf " . $datastore_path . "/" . $cid);
     system("rm -rf " . $datastore_path . "/" . $cid);
 
-    print "Executing: rm -f " . $snapshot_path . "/" . $cid . "*\n";
+    $LOG->info("Executing: rm -f " . $snapshot_path . "/" . $cid . "*");
     system("rm -f " . $snapshot_path . "/" . $cid . "*");
 
     HoneyClient::Manager::Database::set_client_deleted($id);
