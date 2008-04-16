3f9f72caac6cd7b07bd9d8169c8f1cd65834eefb
Darien Kindlund <github@kindlund.com>
2008-04-16 21:17:31 +0000

    Updated utility functions.

--- bin/identify_clients.sh
+++ bin/identify_clients.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#DATASTORE_PATH="/vm/clones"
-DATASTORE_PATH="/vm/suspicious"
+DATASTORE_PATH="/vm/clones"
+#DATASTORE_PATH="/vm/suspicious"
 LIST=$(find $DATASTORE_PATH -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
 
 cd ..
 for VM in $LIST ; do
     echo -n $VM
-    perl -Ilib bin/identify_clients.pl $VM && echo " - Not In Database" || echo " - Exists In Database"
+    perl -Ilib bin/identify_client.pl $VM && echo " - Not In Database" || echo " - Exists In Database"
 done
