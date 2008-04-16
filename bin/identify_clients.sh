c8310e1d9841d89e26837eb32963550d411e1973
Darien Kindlund <github@kindlund.com>
2008-04-16 23:44:52 +0000

    Updated utility script.

--- bin/identify_clients.sh
+++ bin/identify_clients.sh
@@ -4,7 +4,6 @@ DATASTORE_PATH="/vm/clones"
 #DATASTORE_PATH="/vm/suspicious"
 LIST=$(find $DATASTORE_PATH -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
 
-cd ..
 for VM in $LIST ; do
     echo -n $VM
     perl -Ilib bin/identify_client.pl $VM && echo " - Not In Database" || echo " - Exists In Database"
